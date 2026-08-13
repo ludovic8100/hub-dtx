3e3a4da848ac59fe9340f2087df6865aabdbc57f
ludovic8100 <Lde@dynassur.be>
2026-08-13 08:16:52 +0200

    supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://tndwonqdbeszkcztkzqe.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZHdvbnFkYmVzemtjenRrenFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MDg1MTcsImV4cCI6MjA5NjI4NDUxN30.jRrq7nV2Y6KSW_JwHbyOSm4lvckTLmVcAAROhq6sSuw'
+const SUPABASE_ANON_KEY = 'sb_publishable_xBt6ZaZGh5trEloyMCNRuA_MN-jesVJ'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
